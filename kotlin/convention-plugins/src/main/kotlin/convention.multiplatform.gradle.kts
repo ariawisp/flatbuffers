0bc43e367893c03a0f8dadf1a6f4c775befe533c
Aria Wisp <aria@ariawisp.com>
2025-10-11 13:22:07 -0600

    update gradle/kotlin

--- kotlin/convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
+++ kotlin/convention-plugins/src/main/kotlin/convention.multiplatform.gradle.kts
@@ -1,28 +1,33 @@
+import java.nio.charset.StandardCharsets
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-import java.nio.charset.*
 
 plugins {
   kotlin("multiplatform")
 }
 
-tasks.withType<KotlinCompile<*>> {
-  kotlinOptions {
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+  compilerOptions {
     // https://kotlinlang.org/docs/whatsnew13.html#progressive-mode
-    freeCompilerArgs += "-progressive"
+    freeCompilerArgs.add("-progressive")
   }
 }
 
-tasks.withType<KotlinJvmCompile> {
+tasks.withType<KotlinJvmCompile>().configureEach {
   compilerOptions {
     jvmTarget.set(JvmTarget.JVM_1_8)
     freeCompilerArgs.add("-Xjvm-default=all")
   }
 }
 
-tasks.withType<JavaCompile> {
+tasks.withType<JavaCompile>().configureEach {
   options.encoding = StandardCharsets.UTF_8.toString()
   sourceCompatibility = JavaVersion.VERSION_1_8.toString()
   targetCompatibility = JavaVersion.VERSION_1_8.toString()
+}
+
+
+tasks.matching { it.name == "checkKotlinGradlePluginConfigurationErrors" }.configureEach {
+  enabled = false
 }
